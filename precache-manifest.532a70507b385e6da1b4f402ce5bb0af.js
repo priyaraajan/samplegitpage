b9d43774f35149e1f508ac1f63137d5e9550af84
priyarajan sivaperumal <sivaperumalpriyaraajan6@gmail.com>
2019-11-28 22:07:53 +0530

    Updates

---
+++ precache-manifest.532a70507b385e6da1b4f402ce5bb0af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24967ee6d904c72546db84774b69996c",
+    "url": "/samplegitpage/index.html"
+  },
+  {
+    "revision": "91fa02a7648218ab8289",
+    "url": "/samplegitpage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f2c6a3cadc5580dcb20c",
+    "url": "/samplegitpage/static/js/2.07135f52.chunk.js"
+  },
+  {
+    "revision": "91fa02a7648218ab8289",
+    "url": "/samplegitpage/static/js/main.e1a3ca8f.chunk.js"
+  },
+  {
+    "revision": "8ed72cb1c1df8ba8232b",
+    "url": "/samplegitpage/static/js/runtime-main.aba57b82.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/samplegitpage/static/media/logo.25bf045c.svg"
+  }
+]);
